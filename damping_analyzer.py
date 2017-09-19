b1d0204ef0f2493cd70373a0815210dc5b57ac68
joshshep <joshua.adam.shepherd@gmail.com>
2017-09-18 23:40:45 -0700

    Finished simple damping ratio test

--- damping_analyzer.py
+++ damping_analyzer.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 from __future__ import print_function
-from math import sin, pi, cos
+from math import sin, pi, cos, exp, sqrt
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,38 +9,85 @@ class Wave:
 	"""
 	Waves with magnitudes and damping ratios and stimulation points?
 	"""
-	def __init__(self, mag=1.0, freq=1.0, damping_ratio=0.0, start=0.0, start_angle=0.0):
+	def __init__(self, mag=1.0, undamped_freq=1.0, damping_ratio=0.0, start=0.0, start_angle=0.0, color=None):
+		assert(mag>=0)
+		assert(undamped_freq>=0)
+		assert(damping_ratio>=0)
 		self.mag = mag
-		self.freq = freq
+		self.undamped_freq = undamped_freq
 		self.damping_ratio = damping_ratio
 		self.start = start
 		self.start_angle = start_angle
+		self.color = color
+		
+		# the frequency when underdamped is *not* the same as the resonance freq
+		if self.damping_ratio <= 1:
+			# thank you wikipedia: https://en.wikipedia.org/wiki/Harmonic_oscillator#Damped_harmonic_oscillator
+			self.underdamped_freq = self.undamped_freq * sqrt(1 - self.damping_ratio**2)
+			
 	def at(self, t):
 		if t >= self.start:
-			cycle = self.freq * t
-			return self.mag * cos(cycle*2.0*pi + self.start_angle)
+			if self.damping_ratio <= 1:
+				#underdamped
+				#return exp(-1.0*self.undamped_freq*self.damping_ratio*t) * self.mag * cos( (self.underdamped_freq*t + self.start_angle)*2.0*pi )
+				return exp(-1.0*self.undamped_freq*2*pi*self.damping_ratio*t) * self.mag * cos( (self.underdamped_freq*t + self.start_angle)*2.0*pi )
+			else:
+				#critcally or over damped
+				return self.mag * exp(-1.0*2*pi*(1/self.damping_ratio)*t)
 		return 0.0
+	
+	def get_latex_eq(self):
+		pass
+		#return r'$e^-1$'
 
 class Waves:
 	"""
 	A collection of waves
 	"""
 	waves = []
+	
 	def __init__(self, waves):
 		self.waves += waves
+	
 	def at(self, t):
 		tot = 0.0
 		for wave in self.waves:
 			tot += wave.at(t)
 		return tot
+	
 	def rm_dead_waves(self):
 		pass
+	
 	def add_wave(self, w):
 		waves.append(w)
-	def plot(self, t0=0.0, t1=5.0, sampling_rate=20.0):
+	
+	'''
+	
+	Plotting functions
+	
+	'''
+	def plot_dft(self, t0=0.0, t1=5.0, sampling_rate=20.0, color=None):
+		x = np.arange(t0, t1, 1.0/sampling_rate)
+		y = np.fft.ifft(x)
+	def plot_super(self, t0=0.0, t1=5.0, sampling_rate=20.0, color=None):
 		x = np.arange(t0, t1, 1.0/sampling_rate)
 		y = np.ndarray(x.shape)
 		for i in range(x.shape[0]):
 			y[i] = self.at(x[i])
-		plt.plot(x,y)
-		plt.show()
+		plt.plot(x,y, color=color, label='super')
+	
+	def plot_indi(self, t0=0.0, t1=5.0, sampling_rate=20.0):
+		x = np.arange(t0, t1, 1.0/sampling_rate)
+		y = np.ndarray(x.shape)
+		for wave in self.waves:
+			for i in range(x.shape[0]):
+				y[i] = wave.at(x[i])
+			plt.plot(x, y, color=wave.color, label=r'$\zeta = $'+str(wave.damping_ratio))
+	
+	
+	
+	
+	
+	
+	
+	
--- simple_superposition.py
+++ simple_superposition.py
@@ -2,10 +2,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from damping_analyzer import Wave, Waves
+from math import pi
 
 if __name__ == '__main__':
+	'''
 	waves = Waves([
 		Wave(freq=1.3),
 		Wave(freq=7, mag=0.25)
 	])
-	waves.plot(sampling_rate=100)
+	'''
+	'''
+	waves = Waves([
+		Wave(freq=1,   damping_ratio=1),
+		Wave(freq=1.0, damping_ratio=1),
+		Wave(freq=0.2, damping_ratio=1),
+	])
+	'''
+	waves = Waves([
+		Wave(undamped_freq=10/(2*pi), damping_ratio=1, color='black'),
+		Wave(undamped_freq=10/(2*pi), damping_ratio=.5),
+		Wave(undamped_freq=10/(2*pi), damping_ratio=.1),
+		Wave(undamped_freq=10/(2*pi), damping_ratio=2),
+		#Wave(undamped_freq=1.0, damping_ratio=0),
+	])
+	waves.plot_indi(sampling_rate=100, t1=1.6)
+	#waves.plot_super(sampling_rate=100, t1=1.6)
+	#waves.plot_dft()
+	plt.xlabel(r'$\omega t$')
+	plt.ylabel(r'$x(t)\//\/x(0)$')
+	plt.axhline(y=0, linewidth=0.5, color='black')
+	plt.legend()
+	
+	plt.savefig('hyperphysics_similar.png')
+	plt.show()
