3c421b17e91f92f669ec290bd8ee6183a8be9471
joshshep <joshua.adam.shepherd@gmail.com>
2017-09-23 11:06:17 -0700

    ...

--- damping_analyzer.py
+++ damping_analyzer.py
@@ -30,7 +30,10 @@ import matplotlib.pyplot as plt
 class Wave:
     """
     Waves with magnitudes and damping ratios and stimulation points?
+    
+    Input angles are in Hz (they are immediately translated to radians)
     """
+    
     def __init__(self,
                     mag=1.0,
                     undamped_freq=1.0,
@@ -42,16 +45,18 @@ class Wave:
         assert(undamped_freq>=0)
         assert(damping_ratio>=0)
         self.mag = mag
-        self.undamped_freq = undamped_freq
+        self.undamped_freq = undamped_freq * 2 * pi
         self.damping_ratio = damping_ratio
         self.start_t = start_t
-        self.phase = phase
+        self.phase = phase * 2 * pi
         self.color = color
         
         # the frequency when underdamped is *not* the same as the resonance freq
         if self.damping_ratio <= 1:
             # thank you wikipedia: https://en.wikipedia.org/wiki/Harmonic_oscillator#Damped_harmonic_oscillator
             self.underdamped_freq = self.undamped_freq * sqrt(1 - self.damping_ratio**2)
+
+
     '''
     Get the value of the wave at t seconds
     '''
@@ -60,17 +65,35 @@ class Wave:
             t -= self.start_t
             if self.damping_ratio <= 1:
                 #underdamped
+                #https://sites.math.washington.edu/~king/coursedir/m308a01/Projects/m308a01-pdf/reed.pdf
                 #return exp(-1.0*self.undamped_freq*self.damping_ratio*t) * self.mag * cos( (self.underdamped_freq*t + self.phase)*2.0*pi )
-                return exp(-1.0*self.undamped_freq*2*pi*self.damping_ratio*t) * self.mag * cos( (self.underdamped_freq*t + self.phase)*2.0*pi )
+                return exp(-1.0*self.undamped_freq*self.damping_ratio*t) * self.mag * cos( self.underdamped_freq*t + self.phase )
             else:
                 #critcally or over damped
-                return self.mag * exp(-1.0*2*pi*(self.phase +(1/self.damping_ratio))*t)
+                return self.mag * exp(-1.0*(self.phase +(1/self.damping_ratio))*t)
         return 0.0
+    def xdot_at(self, t):
+        if t >= self.start_t:
+            t -= self.start_t
+            if self.damping_ratio <= 1:
+                #underdamped
+                w_0  = self.undamped_freq
+                w_d  = self.underdamped_freq
+                A    = self.mag
+                zeta = self.damping_ratio
+                phi  = self.phase
+                return -1.0 * A * exp(-1.0*w_0*zeta*t) * ( zeta*cos(w_d*t + phi) + w_d*sin(w_d*t + phi) )
+            else:
+                #critcally or over damped
+                return self.mag * exp(-1.0*(self.phase +(1/self.damping_ratio))*t)
+        return 0.0
+        
     def ats(self, ts):
         y = np.ndarray(ts.shape[0])
         for i in range(ts.shape[0]):
             y[i] = self.at(ts[i])
         return y
+
     def get_latex_eq(self):
         pass
         #return r'$e^-1$'
@@ -113,6 +136,7 @@ class Waves:
         #plt.plot(, s.real, label='real')
         plt.plot(freqs, s.real, 'b-', label='real')
         plt.plot(freqs, s.imag, 'r--', label='imaginary')
+
     def plot_super(self, t0=0.0, t1=5.0, sampling_rate=20.0, color=None):
         x = np.arange(t0, t1, 1.0/sampling_rate)
         y = np.ndarray(x.shape)
@@ -120,15 +144,25 @@ class Waves:
             y[i] = self.at(x[i])
         plt.plot(x,y, color=color, label='super')
     
-    def plot_indi(self, t0=0.0, t1=5.0, sampling_rate=20.0):
+    def plot_indi(self, ax, t0=0.0, t1=5.0, sampling_rate=20.0):
         x = np.arange(t0, t1, 1.0/sampling_rate)
         y = np.ndarray(x.shape)
         for wave in self.waves:
             for i in range(x.shape[0]):
                 y[i] = wave.at(x[i])
-            plt.plot(x, y,
-                        color=wave.color,
-                        label=r'$\zeta = $'+str(wave.damping_ratio))
+            ax.plot(x, y,
+                     color=wave.color,
+                     label=r'$\zeta = $'+str(wave.damping_ratio))
+
+    def plot_indi_xdot(self, ax, t0=0.0, t1=5.0, sampling_rate=20.0):
+        x = np.arange(t0, t1, 1.0/sampling_rate)
+        y = np.ndarray(x.shape)
+        for wave in self.waves:
+            for i in range(x.shape[0]):
+                y[i] = wave.xdot_at(x[i])
+            ax.plot(x, y,
+                     color=wave.color,
+                     label=r'$\zeta = $'+str(wave.damping_ratio))
     
     
     
--- simple_estimate_damp.py
+++ simple_estimate_damp.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from damping_analyzer import Wave, Waves
 from math import pi
 from scipy import signal
+import matplotlib.gridspec as gridspec
 
 if __name__ == '__main__':
     '''
@@ -22,23 +23,87 @@ if __name__ == '__main__':
     wave = Wave(undamped_freq=1, damping_ratio=0.145)
     sampling_rate = 100
     t1 = 5
-    x = np.arange(0, 5, 1.0/sampling_rate)
+    x = np.arange(0, t1, 1.0/sampling_rate)
     y = wave.ats(x)
     peakind = signal.find_peaks_cwt(y, np.arange(1,100))
     print ('peakind: {}'.format(peakind))
     print ('x[peakind]: {}'.format(x[peakind]))
     print ('y[peakind]: {}'.format(y[peakind]))
-    waves = Waves([
-        wave
-        #Wave(undamped_freq=1.0, damping_ratio=0),
-    ])
-    waves.plot_indi(sampling_rate=100, t1=t1)
+    
+    x_fmt_gen_eq = r'$x(t) = A e^{-\omega_0 \zeta t} \cos( \omega_d t )$'
+
+    #############
+    # UNDERdamped x'(t)
+    #fmt_gen_eq = r'$\dot{x}(t) = -\omega_0 \zeta A e^{-\omega_0 \zeta t} \cos( \omega_0 \sqrt{1-\zeta^2} t ) - \omega_0 \sqrt{1-\zeta^2} A e^{-\omega_0 \zeta t} \sin( \omega_0 \sqrt{1-\zeta^2} t )$'
+    #fmt_gen_eq = r'$\dot{x}(t) = -\omega_0 A e^{-\omega_0 \zeta t} (\zeta \cos( \omega_0 \sqrt{1-\zeta^2} t ) + \omega_0 \sqrt{1-\zeta^2} \sin( \omega_0 \sqrt{1-\zeta^2} t ))$'
+    #fmt_gen_eq = r'$\dot{x}(t) = -\omega_0 A e^{-\omega_0 \zeta t} (\zeta \cos( \omega_d t ) + \omega_d \sin( \omega_d t ))$'
+    
+    xdot_fmt_gen_eq = r'$\dot{x}(t) = -\omega_0 A e^{-\omega_0 \zeta t} (\zeta \cos( \omega_d t ) + \omega_d \sin( \omega_d t ))$'
+    
+    #############
+    # UNDERdamped x(t)
+    #fmt_gen_eq = r'$x(t) = A e^{-\omega_0 \zeta t} \cos( \omega_0 \sqrt{1-\zeta^2} t )$'
+    
+    #############
+    # OVERdamped x(t)
+    #fmt_gen_eq = r'$ A e^{- \frac{1}{\zeta} t} $'
+    
+    gs0 = gridspec.GridSpec(2,1)
+    tPlot, axes = plt.subplots(nrows=2, ncols=1, sharex=True)
+    
+    freqs = [0.5, 1, 1.5, 2, 2.5, 3, 3.3, 4, 5, 8]
+    
+    for undamped_freq in freqs:
+        
+        waves = Waves([
+            Wave(undamped_freq=undamped_freq, damping_ratio=0.05),
+            Wave(undamped_freq=undamped_freq, damping_ratio=0.2),
+            Wave(undamped_freq=undamped_freq, damping_ratio=0.5),
+            Wave(undamped_freq=undamped_freq, damping_ratio=0.8)
+            #Wave(undamped_freq=1.0, damping_ratio=0),
+        ])
     #waves.plot_super(sampling_rate=100, t1=1.6)
     #waves.plot_dft()
-    plt.xlabel(r'$\omega t$')
-    plt.ylabel(r'$x(t)\//\/x(0)$')
-    plt.axhline(y=0, linewidth=0.5, color='black')
-    plt.legend()
     
-    plt.savefig('peakfind.png')
+    
+        
+        
+        waves.plot_indi(axes[0], sampling_rate=sampling_rate, t1=t1)
+        
+        
+        axes[0].set_title(x_fmt_gen_eq, fontsize=18)
+        #axes[0].set_xlabel(r'$\omega t$', fontsize=14)
+        axes[0].set_ylabel(r'$x(t)\//\/x(0)$', fontsize=14)
+        axes[0].axhline(y=0, linewidth=0.5, color='black')
+        axes[0].legend(fontsize=12)
+        
+        waves.plot_indi_xdot(axes[1], sampling_rate=sampling_rate, t1=t1)
+        
+        
+        axes[1].set_title(xdot_fmt_gen_eq, fontsize=18)
+        axes[1].set_xlabel(r'$\omega t$', fontsize=14)
+        axes[1].set_ylabel(r'$x(t)\//\/x(0)$', fontsize=14)
+        axes[1].axhline(y=0, linewidth=0.5, color='black')
+        axes[1].legend(fontsize=12)
+        
+        plt.savefig('plt_indi_w0_{}.png'.format(undamped_freq))
+        axes[0].clear()
+        axes[1].clear()
     plt.show()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
